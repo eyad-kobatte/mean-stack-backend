16633f22890072c1f2a656edf87ab2e96ee35a0c
Eyad Kobatte <eyad.kobatte@meltwater.com>
2020-08-27 20:13:21 +0530

    clean code up

--- index.js
+++ index.js
@@ -1,49 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const jwt = require('jsonwebtoken');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const app = express();
 app.use(express.json());
 
-function validateEmailAndPassword(email, password) {
-  return {
-    email: email,
-    firstName: '',
-    lastName: '',
-    age: 24,
-    hobbies: [''],
-  };
-}
-
-function createJWT(payload) {
-  const exp = new Date();
-  exp.setMinutes(exp.getMinutes() + 1);
-  payload.exp = exp.getTime();
-
-  // We use RS256 because our keys are RSA keys. This would change depending on what keys were used
-  const token = jwt.sign(payload, process.env.JWT_PRIVATE_KEY, {
-    algorithm: 'RS256',
-  });
-  return token;
-}
-
-app.get('/', (req, res) => {
-  const data = { name: 'Eyad' };
-  data.message = 'Hello ' + data.name;
-  res.json(data);
-});
-
-app.post('/', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const user = validateEmailAndPassword(email, password);
-  if (user) {
-    const jwt = createJWT(user);
-    res.json({ token: jwt });
-  } else {
-    res.status(400).json({ message: 'Invalid email or password' });
-  }
-});
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // Heroku listens on environment variable PORT automatically. They set this up internally
 app.listen(process.env.PORT);
