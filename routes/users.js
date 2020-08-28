7c4e2586821a9d7cb4375cb0c3be9aba6ea9d88a
Eyad Kobatte <eyad.kobatte@meltwater.com>
2020-08-28 23:44:56 +0530

    add all routes

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,46 @@ const operations = require('../lib/operations');
 
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await operations.getAllUsers();
+    res.status(200).json(users);
+    return next;
+  } catch (error) {
+    console.error('Failed to get all users', error.message);
+    return next(error);
+  }
+});
+
 router.post('/', async (req, res, next) => {
   try {
     const user = await operations.saveUser(req.body);
-    res.json(user);
+    res.status(200).json(user);
     return next;
   } catch (error) {
     console.error('Failed to create new user', error.message);
+    return next(error);
+  }
+});
+
+router.put('/:email', async (req, res, next) => {
+  try {
+    const user = await operations.editUser(req.params.email, req.body);
+    res.status(200).json(user);
+    return next;
+  } catch (error) {
+    console.error('Failed to edit a user', error.message);
+    return next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const user = await operations.deleteUser(req.params.id);
+    res.status(200).json(user);
+    return next;
+  } catch (error) {
+    console.error('Failed to delete a user', error.message);
     return next(error);
   }
 });
