62825d26839e8fd28aed2c000b7ec380dc62ddd7
Eyad Kobatte <eyad.kobatte@meltwater.com>
2020-08-28 20:49:34 +0530

    login route

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const express = require('express');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
 const app = express();
 app.use(express.json());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // Heroku listens on environment variable PORT automatically. They set this up internally
 app.listen(process.env.PORT);
